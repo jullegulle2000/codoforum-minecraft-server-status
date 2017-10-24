ebb8e98b01f8f713d0c61a39f5408235c67653e2
jullegulle2000 <33057837+jullegulle2000@users.noreply.github.com>
2017-10-24 13:28:50 +0200

    Update serverstatus.info.php

--- serverstatus/serverstatus.info.php
+++ serverstatus/serverstatus.info.php
@@ -1,1 +1,10 @@
+<?php
+  $info['name'] = 'Minecraft Server Status';
+  $info['plugin_type']= 'block';  
+  $info['description'] = 'Displays server information';
+  $info['version'] = '1.x';
+  $info['author'] = "Julle";
+  $info['author_url'] = 'http://dbdsnetwork.tk';
+  $info['license'] = 'MIT License';
+  $info['core'] = '1.x'; 
 
