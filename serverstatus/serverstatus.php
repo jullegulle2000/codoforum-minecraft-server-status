d2b913422921996a8cfa275f6497f0f051949a7e
jullegulle2000 <33057837+jullegulle2000@users.noreply.github.com>
2017-10-24 13:35:07 +0200

    Update serverstatus.php

--- serverstatus/serverstatus.php
+++ serverstatus/serverstatus.php
@@ -4,21 +4,20 @@
 <?php
 $playeronline = file_get_contents('https://minecraft-api.com/api/ping/playeronline.php?ip=mc.dbdsnetwork.tk&port=25565');  
 
-$ip = '192.168.1.245';
-$port = 25565;
+$ip = 'YOUR SERVER IP HERE';
+$port = YOUR SERVER PORT HERE;
 $checkSock = @fsockopen($ip, $port, $empty, $empty, 1);
 if($checkSock !== FALSE)
 {
     echo '<a style="color:green;font-size:12px;" >
-      mc.dbdsnetwork.tk : ONLINE</a>';
+      YOUR SERVER IP HERE : ONLINE</a>';
 }else{
     echo '<a style="color:red;font-size:12px;" >
-      mc.dbdsnetwork.tk : OFFLINE</a> ';
+      YOUR SERVER IP HERE : OFFLINE</a> ';
 }
 ?>
 <html>
 </li>
-
 <li>
 <a>
 </html>
